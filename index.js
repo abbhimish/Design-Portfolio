af9097ecb3da865ce2ce617973e80cb295bd4447
Abhishek <abhi.mish1424@gmail.com>
2024-05-30 13:01:22 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+ 
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    let filePath = __dirname + '/public/index.html';
+
+    //console.log(req.url);
+
+    switch(req.url) {
+        case '/aboutus': 
+            filePath = __dirname + '/public/about.html'; 
+            break;
+
+        default:
+             filePath = __dirname + '/public/index.html'; 
+             break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data, 'utf8');
+
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`server is running at http://${hostname}:${port}`);
+});
